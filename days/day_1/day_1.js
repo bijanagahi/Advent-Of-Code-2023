5796b81b018ff82a1de706d4706df3d3f6620181
Bijan <bijanagahi@users.noreply.github.com>
2023-12-05 23:52:08 -0700

    lololol what the fuck

--- days/day_1/day_1.js
+++ days/day_1/day_1.js
@@ -1,6 +1,6 @@
-
 document.getElementById("input_field").value = my_input;
 solve_both();
+
 
 function solve_both(){
     solve_1();
@@ -24,7 +24,7 @@ function solve_1(){
         (accumulator, currentValue) => accumulator + parseInt(currentValue),
         0,
       );
-    document.getElementById("output_field").value = results.join('\n');
+    document.getElementById("output_field_1").value = results.join('\n');
     document.getElementById("answer_1").innerHTML = answer;
 }
 
@@ -33,18 +33,36 @@ function solve_2(){
     let matcher = /([0-9])|(one|two|three|four|five|six|seven|eight|nine)/g
     let results = []
     for (let line of lines){
-        let digits = line.match(matcher)
-        if (digits.length == 1) {
-            results.push(digits[0] + digits[0]);
+        let matches = line.match(matcher)
+        // start with the first one
+        let first = matches[0]
+        let last = matches.slice(-1)[0] 
+        if (first !== NaN){
+            results.push(maybeInt);
         }
         else{
-            results.push(digits[0] + digits.slice(-1)[0])
+            results.push()
         }
+
+        
     }
     const answer = results.reduce(
         (accumulator, currentValue) => accumulator + parseInt(currentValue),
         0,
       );
-    document.getElementById("output_field").value = results.join('\n');
-    document.getElementById("answer_1").innerHTML = answer;
+    document.getElementById("output_field_2").value = results.join('\n');
+    document.getElementById("answer_2").innerHTML = answer;
+}
+
+function parseString(input){
+    let char_code = ((input.charCodeAt(0)) ^ (input.charCodeAt(1)))
+    return ( 1*(char_code === 1 )) +
+    ( 2*(char_code === 3 )) +
+    ( 3*(char_code === 28)) +
+    ( 4*(char_code === 9 )) +
+    ( 5*(char_code === 15)) +
+    ( 6*(char_code === 26)) +
+    ( 7*(char_code === 22)) +
+    ( 8*(char_code === 12)) +
+    ( 9*(char_code === 7 ))
 }
